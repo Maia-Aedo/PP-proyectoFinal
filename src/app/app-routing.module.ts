c89b6a75f41646039162a54ec05d9bbbbcc94628
Maia-Aedo <aedomaia10@gmail.com>
2022-12-07 13:50:38 -0300

    actualizacion-error en login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'inicio', component: HomeComponent },
+  { path: 'equipo', component: EquipoComponent },
+  { path:"contacto", component:ContactoComponent },
+  { path: 'iniciar-sesion', component: LoginComponent },
+  { path: 'empleados', component: EmpleadosComponent },
+  { path: 'lista-empleados', component: EmpleadosComponent },
+  { path: 'agregar-empleado', component: CrearEmpleadoComponent },
+  { path: 'crear-empleado', component: CrearEmpleadoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'proyectofinal-96e45',
+    appId: '1:946470241333:web:c6c53827fec3a9d4499d7c',
+    storageBucket: 'proyectofinal-96e45.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA8kM9JitmBATptWuZSKwhgNnk0741Q5Mg',
+    authDomain: 'proyectofinal-96e45.firebaseapp.com',
+    messagingSenderId: '946470241333',
+  },
+  production: false,
+  // configuracion de firebase, si no se configura no funcionara
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
